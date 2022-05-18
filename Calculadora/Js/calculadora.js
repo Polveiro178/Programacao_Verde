887406b25320b60d5f289c8e45bd2ed719c10b0d
Polveiro178 <105284731+Polveiro178@users.noreply.github.com>
2022-05-18 09:51:04 -0300

    Add files via upload

--- Calculadora/Js/calculadora.js
+++ Calculadora/Js/calculadora.js
@@ -1,7 +1,34 @@
-alert("Caralho Feião tu hein")
-function acao() 
-{
-    document.getElementById("botao").innerHTML = "Acha que isso funciona? KKKK"  
-    alert("Chorei")
-} 
+var btn = document.querySelectorAll(".key li"),
+    input = document.querySelector(".input-valor"),
+    operador = document.querySelectorAll(".operador");
 
+for (var i = 0; i < btn.length; i++) {
+    document.onkeypress = function(event) {
+        for (var e = 0; e <= 10; e++) {
+            if (key === (48 + e)) {
+                input.innerHTML += e;
+            }
+        }
+        switch (key) {
+            case 1:
+                input.innerHTML += "*";
+                break;
+            case 2:
+                input.innerHTML += "+";
+                break;
+            case 3:
+                input.innerHTML += "-";
+                break;
+            case 4:
+                input.innerHTML += ".";
+                break;
+            case 5:
+                input.innerHTML += "/";
+                break;
+            case 6:
+            case 7:
+                var equacao = input.innerHTML;
+        }
+
+    }
+}
