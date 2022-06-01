d3aed28461823823968a24db6d2ee3fe021b4626
Polveiro178 <105284731+Polveiro178@users.noreply.github.com>
2022-06-01 08:15:22 -0300

    Add files via upload

---
+++ Média/média.js
@@ -0,0 +1,12 @@
+function media(){
+    var nota1 = parseFloat(document.getElementById("nota1").value);
+    var nota2 = parseFloat(document.getElementById("nota2").value);
+
+    var media = (nota1 + nota2) /2;
+
+    if(media>=7)
+    if(media==10)
+    alert("Ih ala o cara perdeu mó tempo")
+    else
+    alert("Reprovou Otário KKK")
+}
